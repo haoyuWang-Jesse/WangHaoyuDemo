77ab7aa0492a3158b9f2dd1dd4e81e48fedd35e7
haoyu3 <haoyu3@staff.sina.com.cn>
2016-05-24 12:10:13 +0800

    Initial Commit

---
+++ WangHaoyu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WangHaoyu
+//
+//  Created by haoyu3 on 16/5/24.
+//  Copyright © 2016年 haoyu3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WangHaoyu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WangHaoyu
+//
+//  Created by haoyu3 on 16/5/24.
+//  Copyright © 2016年 haoyu3. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
